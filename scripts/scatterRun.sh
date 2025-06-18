5455d70157b0833ee19c5344b1c5b86e5965c95e
juniorsohn <juniorsohn@hotmail.com>
2025-06-18 09:50:07 -0300

    mudancas finais a principio, coletando dados pra rodar experimentos.

--- scripts/scatterRun.sh
+++ scripts/scatterRun.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ARQUIVO="../data/teste.csv"
+ARQUIVO="../data/eventos.csv"
 BINARIO="./main"
 REPETICOES=10
+HOSTS="hosts.txt"
+INTERFACE="10.20.221.0/24"
+NP=$1 #recebe via linha de comando
 
 # Algoritmos do Scatter: ID e nome
 declare -A ALGORITMOS=(
@@ -14,22 +17,28 @@ declare -A ALGORITMOS=(
 
 # Arquivo de saída
 OUT="resultados_scatter.csv"
-echo "algoritmo,tempo" > "$OUT"
+
+# Se o arquivo ainda não existe, adiciona o cabeçalho
+if [[ ! -f "$OUT" ]]; then
+    echo "algoritmo,tempo,np" > "$OUT"
+fi
 
 for id in 0 1 2 3; do
     nome=${ALGORITMOS[$id]}
-    echo "Rodando algoritmo $nome (ID $id)..."
+    echo "Rodando algoritmo $nome (ID $id) com np=$NP..."
 
     for i in $(seq 1 $REPETICOES); do
         tempo=$(
-        mpirun -np 1 \
-            --mca coll_tuned_use_dynamic_rules 1 \
-            --mca coll_tuned_scatter_algorithm $id \
-            $BINARIO $ARQUIVO 2>/dev/null | \
+        mpirun --machinefile "$HOSTS" \
+               --mca btl_tcp_if_include "$INTERFACE" \
+               --mca coll_tuned_use_dynamic_rules 1 \
+               --mca coll_tuned_scatter_algorithm $id \
+               -np $NP \
+               "$BINARIO" "$ARQUIVO" 2>/dev/null | \
         grep "Tempo total de execução" | awk '{print $(NF-1)}')
 
         if [[ ! -z "$tempo" ]]; then
-            echo "$nome,$tempo" >> "$OUT"
+            echo "$nome,$tempo,$NP" >> "$OUT"
         else
             echo "Falha ao extrair tempo na execução $i (algoritmo $id)"
         fi
